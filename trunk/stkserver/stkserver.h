f3c9a9bbbde25d74d0b155edef533f7fad6dc9f3
hzl6255@gmail.com <hzl6255@gmail.com@dd9a0a00-37c7-8521-0e3b-9ff89e2e57bb>
2014-01-17 09:09:29 +0000

    加入cJSON来解析服务器端用户存储文件 | HuZhilin

--- trunk/stkserver/stkserver.h
+++ trunk/stkserver/stkserver.h
@@ -12,10 +12,18 @@
 #define STK_SERVER_PORT 9007
 #define STK_MAX_CLIENTS 30
 #define STK_MAX_PACKET_SIZE 65535
+#define STK_PASS_SIZE 64
+#define STK_DEFAULT_SIZE 64
+
+#define STK_USER_FILE "users"
 
 typedef struct{
     int fd;
     unsigned int stkc_uid;
+    unsigned char stkc_pass[STK_PASS_SIZE];
+    unsigned char stkc_city[STK_DEFAULT_SIZE];
+    unsigned int stkc_phone;
+    unsigned char stkc_gender;
 }stk_client;
 
 #endif /* _STKSERVER_H_ */
