f8bd1aabe77350617a058e6f5515337ca09f75e7
hzl6255 <hzl6255@gmail.com>
2014-02-26 09:39:40 +0000

    消除Linux下STK Server/Client编译Warning

--- stkserver/stk.h
+++ stkserver/stk.h
@@ -15,19 +15,10 @@
 
 /*
  *********************************************
- *               stkuser.c                   *
- *********************************************
- */
-int stk_init_user();
-stk_client *stk_find_user(unsigned int uid);
-stk_client *stk_find_user(unsigned int uid);
-
-/*
- *********************************************
  *              stkpacket.c                  *
  *********************************************
  */
-int stk_server_socket();
+int stk_server_socket(void);
 stk_client *stk_parse_packet(char *buf, int size, stk_data *data);
 int stk_reqlogin_ack(int fd, unsigned int uid, char *buf);
 int stk_login_ack(int fd, unsigned int uid, char *buf);
@@ -37,6 +28,18 @@ int stk_getonlineuser_ack(stk_client *client, char *buf);
 int stk_getinfo_ack(stk_client *client, char *buf);
 int stk_sendmsg_ack(stk_client *client, char *buf, int bytes);
 void stk_handle_signal(int signal);
+
+
+/*
+ *********************************************
+ *               stkuser.c                   *
+ *********************************************
+ */
+int stk_init_user(void);
+stk_client *stk_find_user(unsigned int uid);
+stk_client *stk_find_user(unsigned int uid);
+stk_client *stk_get_next(stk_client *client);
+stk_client *stk_get_user_by_tid(pthread_t tid);
 
 
 #endif /* _STK_H_ */
